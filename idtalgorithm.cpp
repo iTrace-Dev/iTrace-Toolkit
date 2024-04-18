beee0d2b70f99b249ce705ea065cb8d508b0ccab
Joshua Behler <Joshua2000B@gmail.com>
2024-04-18 11:42:57 -0400

    Replace IDT algorithm with new version - not tested yet

--- idtalgorithm.cpp
+++ idtalgorithm.cpp
@@ -32,43 +32,68 @@ QVector<Fixation> IDTAlgorithm::generateFixations() {
 
     //This code follows the IDT Algorithm
 
-    //Step 1 should already be done
-
-    //Step 2 - Calculate velocity between each point and generate Fixations
-    QVector<Gaze> window;
-    int i = 0;
-
-    while(i < duration_window && i < session_gazes.size()) {
-        window.push_back(session_gazes[i]);
-        ++i;
-    }
-
-    while(i < session_gazes.size()) {
-        if((computeGazeDifference(window) <= dispersion) && (window.size() >= duration_window)) {
-            while(computeGazeDifference(window) <= dispersion) {
-                if(i < session_gazes.size() - 1) {
-                    window.push_back(session_gazes[i]);
-                    ++i;
-                }
-                else { break; }
-            }
-            fixations.push_back(computeFixationEstimate(window));
-            window.clear();
+    //QVector<Gaze> window;
+    while(session_gazes.size() != 0) { // While there are still points
+        // Initialize the window over the first points to not cover the duration threshold
+        QVector<Gaze> window;
+        int i = 0;
+        while(window.size() != duration_window && session_gazes.size() != 0) {
             window.push_back(session_gazes[i]);
             ++i;
         }
-        else if(window.size() < duration_window) {
-            window.push_back(session_gazes[i]);
-            ++i;
-        }
-        else {
-            window.pop_front();
-            if(window.size() < duration_window) {
+
+        // If dispersion of window points <= threshold
+        if(computeGazeDifference(window) <= dispersion) {
+            // Add additional points to the window until dispersion > threshold
+            while(computeGazeDifference(window) <= dispersion && i < session_gazes.size()) {
                 window.push_back(session_gazes[i]);
                 ++i;
             }
+            // Not a fixation ant the centroid of the windows points
+            fixations.push_back(computeFixationEstimate(window));
+            // Remove window points from points
+            for(int x = 0; x < window.size(); ++x) {
+                session_gazes.pop_front();
+            }
+        }
+        else {
+           session_gazes.pop_front();
         }
     }
+//    QVector<Gaze> window;
+//    int i = 0;
+
+//    while(i < duration_window && i < session_gazes.size()) {
+//        window.push_back(session_gazes[i]);
+//        ++i;
+//    }
+
+//    while(i < session_gazes.size()) {
+//        if((computeGazeDifference(window) <= dispersion) && (window.size() >= duration_window)) {
+//            while(computeGazeDifference(window) <= dispersion) {
+//                if(i < session_gazes.size() - 1) {
+//                    window.push_back(session_gazes[i]);
+//                    ++i;
+//                }
+//                else { break; }
+//            }
+//            fixations.push_back(computeFixationEstimate(window));
+//            window.clear();
+//            window.push_back(session_gazes[i]);
+//            ++i;
+//        }
+//        else if(window.size() < duration_window) {
+//            window.push_back(session_gazes[i]);
+//            ++i;
+//        }
+//        else {
+//            window.pop_front();
+//            if(window.size() < duration_window) {
+//                window.push_back(session_gazes[i]);
+//                ++i;
+//            }
+//        }
+//    }
 
     return fixations;
 }
